1b807b0ce969dd330a1c18d7d777dd5c731d9eff
pigsye <eayancheong@gmail.com>
2025-02-19 22:28:44 +0800

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base URL for the API
-export const BASE_URL = "http://127.0.0.1:5000";
+export const BASE_URL = "http://54.179.178.177:5000";
 
 const API = axios.create({
     baseURL: BASE_URL + "/api/user",
